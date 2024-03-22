d585cdc11e965cb510d6879e3ce0fd34ffd9f5e8
RPPython <pr79@abv.bg>
2024-03-22 16:57:56 +0200

    Task12

--- hw_Lab6.py
+++ hw_Lab6.py
@@ -297,4 +297,24 @@
 
 
 
+# ================= Task 12 ================
+# Write a program that outputs a numerical triangle plot
+# the following sequence:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4 5 6
+# 1 2 3 4 5 6 7
+# 1 2 3 4 5 6 7 8
+# 1 2 3 4 5 6 7 8 9
 
+# def numerical_triangle(rows):
+#     for i in range(1, rows + 1):
+#         for j in range(1, i + 1):
+#             print(j, end=" ")
+#         print()
+
+# num_rows = 9
+# numerical_triangle(num_rows)
